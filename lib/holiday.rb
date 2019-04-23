dd3f9fb1ad25ab85e192d8ca1db1e9f15b845923
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-23 15:07:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,9 +65,9 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   new_array = []
   holiday_hash.each do|holiday, supplies|
-    puts "#{holiday.to_s.capitalize}:"
+    # puts "#{holiday.to_s.capitalize}:"
     supplies.each do |sup,sup1|
-      puts " #{holiday.to_s.capitalize}: #{sup1.join(",")}"
+      puts " #{sup.to_s.capitalize}: #{sup1.join(",")}"
     end
   end
   new_array
