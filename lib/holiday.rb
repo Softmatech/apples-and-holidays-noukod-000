4496056a2f2095856afb97fa53a3733a426a7ed7
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-23 15:51:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,7 +79,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season,holidays|
       holidays.map do|holiday,data|
-      holiday if data.include?("BBQ")
+      holidays if data.include?("BBQ")
     end
   end
 end
