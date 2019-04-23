ad5ec910235105916b6b789a42470fa604db93b3
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-23 15:09:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do|holiday, supplies|
     puts "#{holiday.to_s.capitalize}:"
     supplies.each do |sup,sup1|
-      puts "  #{sup.to_s.capitalize}: #{sup1.join(",")}"
+      ss = " #{sup1}"
+      puts "  #{sup.to_s.capitalize}: #{ss.join(",")}"
     end
   end
   new_array
