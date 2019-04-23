6a8588af04009047fa9bcd7cb3262a1db541317e
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-23 14:56:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   new_array = []
   holiday_hash.each do|holiday, supplies|
-      new_array << supplies
+      new_array << holiday
   end
   new_array
 end
